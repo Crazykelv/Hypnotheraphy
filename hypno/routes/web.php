3a81d7687c277c529c60347beba89a6ece35bd57
Fahri Aminuddin Abdillah <fahriaminnudin@gmail.com>
2024-02-18 19:50:44 +0700

    Adding minor comments

--- hypno/routes/web.php
+++ hypno/routes/web.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-
+// Home Controller
 Route::get('/', [HomeController::class, 'wlc']);
 Route::get('dashboard', [HomeController::class, 'dashboard']);
 
-
+// Autentikasi Controller
 Route::get('register', [Autentikasi::class, 'register']);
 Route::get('login', [Autentikasi::class, 'login']);
 Route::get('logout', [Autentikasi::class, 'logout']);
